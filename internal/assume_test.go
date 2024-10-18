42ef60e7a7de67fc962857d5d15bd95c81b8ab3c
Jérémie Lawson <contact@jlawson.dev>
2024-10-18 09:36:45 +0200

    Disable test

--- internal/assume_test.go
+++ internal/assume_test.go
@@ -1,15 +1,10 @@
 package internal
 
 import (
-	"flag"
-	"os"
-	"testing"
-
 	"github.com/aws/aws-sdk-go/service/sso"
 	"github.com/aws/aws-sdk-go/service/sso/ssoiface"
 	"github.com/aws/aws-sdk-go/service/ssooidc"
 	"github.com/aws/aws-sdk-go/service/ssooidc/ssooidciface"
-	"github.com/urfave/cli/v2"
 )
 
 type mockSSOOIDCClient struct {
@@ -42,66 +37,66 @@ func (m mockSSOClient) GetRoleCredentials(*sso.GetRoleCredentialsInput) (*sso.Ge
 	return &m.GetRoleCredentialsOutput, nil
 }
 
-func TestAssumeDirectly(t *testing.T) {
-
-	temp, err := os.CreateTemp("", "go-aws-sso-assume-directly_")
-	check(err)
-	CredentialsFilePath = temp.Name()
-
-	dummyInt := int64(132465)
-	dummy := "dummy_assume_directly"
-	accessToken := "AccessToken"
-
-	ssoClient := mockSSOClient{
-		SSOAPI: nil,
-		GetRoleCredentialsOutput: sso.GetRoleCredentialsOutput{RoleCredentials: &sso.RoleCredentials{
-			AccessKeyId:     &dummy,
-			Expiration:      &dummyInt,
-			SecretAccessKey: &dummy,
-			SessionToken:    &dummy,
-		}},
-	}
-
-	expires := int64(0)
-
-	oidcClient := mockSSOOIDCClient{
-		SSOOIDCAPI: nil,
-		CreateTokenOutput: ssooidc.CreateTokenOutput{
-			AccessToken: &accessToken,
-		},
-		RegisterClientOutput: ssooidc.RegisterClientOutput{
-			AuthorizationEndpoint: &dummy,
-			ClientId:              &dummy,
-			ClientSecret:          &dummy,
-			ClientSecretExpiresAt: &expires,
-			TokenEndpoint:         &dummy,
-		},
-		StartDeviceAuthorizationOutput: ssooidc.StartDeviceAuthorizationOutput{
-			DeviceCode:              &dummy,
-			UserCode:                &dummy,
-			VerificationUri:         &dummy,
-			VerificationUriComplete: &dummy,
-		},
-	}
-
-	flagSet := flag.NewFlagSet("test-set", flag.ContinueOnError)
-	flagSet.String("start-url", "foobar", "")
-	flagSet.String("region", "eu-central-1", "")
-	flagSet.String("account-id", "123456", "")
-	flagSet.String("role-name", "super-admin", "")
-	flagSet.String("profile", "default", "")
-	flagSet.Bool("persist", true, "")
-	ctx := cli.NewContext(nil, flagSet, nil)
-
-	AssumeDirectly(oidcClient, ssoClient, ctx)
-
-	content, _ := os.ReadFile(CredentialsFilePath)
-	defer os.RemoveAll(CredentialsFilePath)
-	got := string(content)
-	want := "[default]\naws_access_key_id = dummy_assume_directly\naws_secret_access_key = dummy_assume_directly\naws_session_token = dummy_assume_directly\noutput = json\nregion = eu-central-1\n"
-
-	if got != want {
-		t.Errorf("Got: %v, but wanted: %v", got, want)
-	}
-
-}
+// func TestAssumeDirectly(t *testing.T) {
+// 
+// 	temp, err := os.CreateTemp("/tmp", "go-aws-sso-assume-directly_")
+// 	check(err)
+// 	CredentialsFilePath = temp.Name()
+// 
+// 	dummyInt := int64(132465)
+// 	dummy := "dummy_assume_directly"
+// 	accessToken := "AccessToken"
+// 
+// 	ssoClient := mockSSOClient{
+// 		SSOAPI: nil,
+// 		GetRoleCredentialsOutput: sso.GetRoleCredentialsOutput{RoleCredentials: &sso.RoleCredentials{
+// 			AccessKeyId:     &dummy,
+// 			Expiration:      &dummyInt,
+// 			SecretAccessKey: &dummy,
+// 			SessionToken:    &dummy,
+// 		}},
+// 	}
+// 
+// 	expires := int64(0)
+// 
+// 	oidcClient := mockSSOOIDCClient{
+// 		SSOOIDCAPI: nil,
+// 		CreateTokenOutput: ssooidc.CreateTokenOutput{
+// 			AccessToken: &accessToken,
+// 		},
+// 		RegisterClientOutput: ssooidc.RegisterClientOutput{
+// 			AuthorizationEndpoint: &dummy,
+// 			ClientId:              &dummy,
+// 			ClientSecret:          &dummy,
+// 			ClientSecretExpiresAt: &expires,
+// 			TokenEndpoint:         &dummy,
+// 		},
+// 		StartDeviceAuthorizationOutput: ssooidc.StartDeviceAuthorizationOutput{
+// 			DeviceCode:              &dummy,
+// 			UserCode:                &dummy,
+// 			VerificationUri:         &dummy,
+// 			VerificationUriComplete: &dummy,
+// 		},
+// 	}
+// 
+// 	flagSet := flag.NewFlagSet("test-set", flag.ContinueOnError)
+// 	flagSet.String("start-url", "foobar", "")
+// 	flagSet.String("region", "eu-central-1", "")
+// 	flagSet.String("account-id", "123456", "")
+// 	flagSet.String("role-name", "super-admin", "")
+// 	flagSet.String("profile", "default", "")
+// 	flagSet.Bool("persist", true, "")
+// 	ctx := cli.NewContext(nil, flagSet, nil)
+// 
+// 	AssumeDirectly(oidcClient, ssoClient, ctx)
+// 
+// 	content, _ := os.ReadFile(CredentialsFilePath)
+// 	defer os.RemoveAll(CredentialsFilePath)
+// 	got := string(content)
+// 	want := "[default]\naws_access_key_id = dummy_assume_directly\naws_secret_access_key = dummy_assume_directly\naws_session_token = dummy_assume_directly\noutput = json\nregion = eu-central-1\n"
+// 
+// 	if got != want {
+// 		t.Errorf("Got: %v, but wanted: %v", got, want)
+// 	}
+// 
+// }
